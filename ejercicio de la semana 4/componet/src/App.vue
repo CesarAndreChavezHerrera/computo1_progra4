89b2af62dfd2230ee2ab9711094304dd92f7826d
cesar chavez <cesar.a.chavez.h@gmail.com>
2022-02-08 09:16:06 -0600

    inicio de la semana 4

--- ejercicio de la semana 4/componet/src/App.vue
+++ ejercicio de la semana 4/componet/src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
-    <!--HelloWorld msg="Welcome to Your Vue.js App"/-->
-    <fistComponet/>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import fistComponet from './components/fistComponet.vue'
-//import HaaelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-   // HelloWorld,
-    fistComponet
+    HelloWorld
   }
 }
 </script>
