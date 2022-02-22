43fb0b9acf0b5d03277bba098ea5ff7db1fab82b
Cesar Chavez <65314179+CesarAndreChavezHerrera@users.noreply.github.com>
2022-02-22 09:21:39 -0600

    semana 5

---
+++ ejercicio de la semana 5/app/src/views/ListEmployee.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <h1>Listado de empleados</h1>
+    <employee/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Employee from '@/components/Employee.vue'
+
+
+export default {
+  name: 'ListEmployee',
+  components: {
+    Employee
+  
+  }
+}
+</script>
